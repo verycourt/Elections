d25da50ffbd663af85d1746ef5bcfc6ce67cd812
Davy Bensoussan <davy.bensoussan@telecom-paristech.fr>
2017-01-17 23:43:00 +0100

    1er jet pour le graphique

--- Gtrends/data/trendsAPI.py
+++ Gtrends/data/trendsAPI.py
@@ -174,10 +174,14 @@ class TrendReq(object):
         for row in self.results['table']['rows']:
             row_dict = {}
             for i, value in enumerate(row['c']):
-                row_dict[headers[i]] = value['f']
+                try:
+                    row_dict[headers[i]] = value['f']
+                except:
+                    continue
             df = df.append(row_dict, ignore_index=True)
-        # df['Date'] = pd.to_datetime(df['Date'])
-        df.set_index('Date', inplace=True)
+        df.dropna(axis=0, how='any', inplace=True) # dropping NaN values
+        
+        #df.set_index('Date', inplace=True)
         self.results = df
         return self.results
 
