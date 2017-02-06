7dc3862833fed97bc0ffaab16a946ba7de266fca
Davy Bensoussan <davy.bensoussan@telecom-paristech.fr>
2017-02-06 16:30:49 +0100

    header site internet

--- site/gtrends/themes.php
+++ site/gtrends/themes.php
@@ -5,13 +5,13 @@
 		<meta charset="UTF-8"></meta>
 		<link rel="stylesheet" href="style.css" />
 		<title>Google Trends</title>
+
 	</head>
 
 	<body>
 		<h2>Recherches Google sur les principaux thèmes de la campagne présidentielle.</h2>
 		<div class="bar">
 			<canvas id="myBarChart" width="600" height="450"></canvas>
-			
 		</div>
 
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js"></script>
