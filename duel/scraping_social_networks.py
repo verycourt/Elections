3c6ecc2c6ee62f73305e87523581146c9fceb7f4
Davy Bensoussan <davy.bensoussan@telecom-paristech.fr>
2017-02-13 21:53:46 +0100

    changement : un json par jour au lieu d'un part candidat

--- duel/scraping_social_networks.py
+++ duel/scraping_social_networks.py
@@ -3,7 +3,7 @@
 
 import sys
 import requests
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 import pandas as pd
 from bs4 import BeautifulSoup
 if sys.version_info[0] == 2:
@@ -86,68 +86,63 @@ app_secret = "ea787efd843d1de746817ec6e9bf7e94"
 access_token = app_id + "|" + app_secret
 google_key = 'AIzaSyBkRrj_kFDUv-T76CJaI3Pd-g3v7UY4GMA'
 
-now = datetime.utcnow() + timedelta(hours=1)
-now = now.replace(minute=0, second=0, microsecond=0) # troncature de la date
+today = date.today()
+print(today)
 
-# path = 'data/' # local save path
-path = '/var/www/html/duel/data/' # AWS save path
+path = 'data/' # save path
+# path = '/var/www/html/duel/data/'
 
-print('Maj du', now)
+df = pd.DataFrame()
+print('Maj du', today)
 
 for candidate in accounts:
     print('-' * 20)
     print(candidate)
     print('-' * 20)
 
-    try: # Load or create DataFrame
-        df = pd.read_json(path + accounts[candidate][3] + '.json', orient='split')
+    stats = {}
+    try: # Twitter : [tweets, following, followers]
+        print('Analyzing Twitter account', accounts[candidate][2])
+        soup = BeautifulSoup(requests.get('https://twitter.com/' + accounts[candidate][2] + '?lang=en').text, 'lxml')
+        stats_tw = [int(tag.attrs['title'].replace(',', '').split(' ')[0])
+                    for tag in soup.find_all(class_='ProfileNav-stat', limit=3) if 'title' in tag.attrs]
     except:
-        df = pd.DataFrame()
+        stats_tw = ['-', '-', '-']
 
-    if now not in df.index:
-        stats = {}
-        try: # Twitter : [tweets, following, followers]
-            print('Analyzing Twitter account', accounts[candidate][2])
-            soup = BeautifulSoup(requests.get('https://twitter.com/' + accounts[candidate][2] + '?lang=en').text, 'lxml')
-            stats_tw = [int(tag.attrs['title'].replace(',', '').split(' ')[0])
-                        for tag in soup.find_all(class_='ProfileNav-stat', limit=3) if 'title' in tag.attrs]
+    stats['tw_tweets'], stats['tw_following'], stats['tw_followers'] = stats_tw
+
+    if accounts[candidate][0] is not None:
+        print('Scanning Youtube Channel')
+        try: # Youtube [abonnés, total vues, nombre de vidéos]
+            stats_yt = YoutubePageData(accounts[candidate][0], google_key)
         except:
-            stats_tw = ['-', '-', '-']
-            
-        stats['tw_tweets'], stats['tw_following'], stats['tw_followers'] = stats_tw
-              
-        if accounts[candidate][0] is not None:
-            print('Scanning Youtube Channel')
-            try: # Youtube [abonnés, total vues, nombre de vidéos]
-                stats_yt = YoutubePageData(accounts[candidate][0], google_key)
-            except:
-                stats_yt = ['-', '-', '-']
-            try: # Youtube [moyenne vues 5 vidéos, moyenne likes 5 vidéos, moyenne dislikes 5 vidéos]
-                stats_yt2 = YoutubeVideosData(accounts[candidate][0], google_key)
-            except:
-                stats_yt2 = ['-', '-', '-']
-        else:
-            print('No Youtube Channel')
-            stats_yt, stats_yt2 = [0, 0, 0], [0, 0, 0]
-
-        stats['yt_subscribers'], stats['yt_views'], stats['yt_videos'] = stats_yt
-        stats['yt_views_avg'], stats['yt_likes_avg'], stats['yt_dislikes_avg'] = stats_yt2
-            
-        try: # Facebook : [likes, people talking about this]
-            stats_fb = FacebookPageData(accounts[candidate][1], access_token)
+            stats_yt = ['-', '-', '-']
+        try: # Youtube [moyenne vues 5 vidéos, moyenne likes 5 vidéos, moyenne dislikes 5 vidéos]
+            stats_yt2 = YoutubeVideosData(accounts[candidate][0], google_key)
         except:
-            stats_fb = ['-', '-']
-            
-        stats['fb_likes'], stats['fb_talking_about'] = stats_fb
-
-        print()
-        print('Collected data')
-        print(stats)
-        print()
-        # ajout de la ligne du jour dans le json
-        rec = pd.DataFrame([stats.values()], columns=stats.keys(), index=[now])
-        df.append(rec, verify_integrity=False).to_json(path + accounts[candidate][3] + '.json', orient='split')
-
+            stats_yt2 = ['-', '-', '-']
     else:
-        print('Donnees deja a jour.')
-        print()
+        print('No Youtube Channel')
+        stats_yt, stats_yt2 = [0, 0, 0], [0, 0, 0]
+
+    stats['yt_subscribers'], stats['yt_views'], stats['yt_videos'] = stats_yt
+    stats['yt_views_avg'], stats['yt_likes_avg'], stats['yt_dislikes_avg'] = stats_yt2
+
+    try: # Facebook : [likes, people talking about this]
+        stats_fb = FacebookPageData(accounts[candidate][1], access_token)
+    except:
+        stats_fb = ['-', '-']
+
+    stats['fb_likes'], stats['fb_talking_about'] = stats_fb
+
+    print()
+    print('Collected data')
+    print(stats)
+    print()
+    
+    # ajout de la ligne du candidat dans le dataframe
+    rec = pd.DataFrame([stats.values()], columns=stats.keys(), index=[accounts[candidate][3]])
+    df = df.append(rec, verify_integrity=False)
+
+# sauvegarde des données
+df.to_json(path + str(today) + '.json', orient='split')
