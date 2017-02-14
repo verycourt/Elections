601f2aec2a8f6267d0f0c1be7e4803bf8845605d
verycourt <bertrand2vericourt@gmail.com>
2017-02-14 18:09:57 +0100

    dashboard global clarifications

--- site/index.php
+++ site/index.php
@@ -11,20 +11,19 @@
 	<body>	
 		<header><?php include "/var/www/html/site_header.html";?></header>
 		<div class="col-md-16 panel panel-default">
-			<div class="panel panel-heading text-center"><h5>Pollster</h5></div>
+			<div class="panel panel-heading text-center"><h2>Pollster</h2></div>
 			<div class="panel panel-body wrapper" style="text-align:center">
 				<link rel="stylesheet" type="text/css" href="1er tour/Pollster_1T_V0.css">
 				<div id="dashboard-pollster">
 					<button  id="button" class="btn-danger btn-responsive" style="text-align:center;padding-bottom:0.2%; 
-					font-size:0.7vw;width:10%;max-height:7%;position:relative;left:35%;bottom:25%;">Tout sélectionner</button>
+					font-size:0.9vw;width:10%;max-height:7%;position:relative;left:35%;bottom:25%;">Tout sélectionner</button>
 					<script src="https://d3js.org/d3.v3.min.js"></script>
 					<script src="1er tour/Pollster_1T_V0.js"></script>
 				</div>				
 			</div>
 		</div>
 		<div class="col-md-16 panel panel-default">
-			<div class="col-md-16 panel-heading text-center">
-				<h5> Second tour : hypothèse Marine Le Pen VS Emmanuel Macron</h5></div>
+			<div class="col-md-16 panel-heading text-center"><h2>Second tour : hypothèse Marine Le Pen VS Emmanuel Macron</h2></div>
 				<div class="col-md-16 panel panel-body" id="dashboard">
 				<script src="2nd tour/graphes.js"> </script>
 				<script>afficher("2nd tour/mlpVSem.tsv")</script>
@@ -33,8 +32,7 @@
                 </div>
 	
 		<div class="col-md-16 panel panel-default">
-			<div class="col-md-16 panel-heading text-center">
-			<h5> Suivi des recherches google par candidat</h5></div>
+			<div class="col-md-16 panel-heading text-center"><h2>Evolution des recherches Google par candidat sur 24h</h2></div>
 			<div class="col-md-16 panel panel-body">
 				<div class="line">
 					<canvas id="myChart" width=900 height=500></canvas>
@@ -46,14 +44,14 @@
 			</div>
 		</div>
 		<div class="col-md-16 panel panel-default">
-			<div class="panel panel-heading text-center"><h5>Décompte de mentions Twitter par candidats sur 3 jours glissants</h5></div>
+			<div class="panel panel-heading text-center"><h2>Décompte de mentions Twitter par candidats sur 3 jours glissants</h2></div>
 			<div id="twittermentions" class="panel panel-body">
 				<script src="decompte/histo_count.js"> </script>
 				<link rel="stylesheet" href="decompte/styledecompte.css">
 			</div>
 		</div>
 		<div class="col-md-16 panel panel-default">
-			<div class="panel panel-heading text-center"><h5>WordCloud Twitter par candidat sur 3 jours</h5></div>
+			<div class="panel panel-heading text-center"><h2>WordCloud Twitter par candidat sur 3 jours glissants</h2></div>
 			<div class="panel panel-body" style="text-align:center">
 				<img width="80%" height="80%" src="decompte/hamon_cloud.png">
 			</div>
