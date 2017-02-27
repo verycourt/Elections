ef85dc821f9c8d31801a21dcdd0dd0ed8f346f5b
rvi008 <bertrand2vericourt@gmail.com>
2017-02-27 15:05:54 +0000

    change pollster data name + started shaping word cloud (WIP)

--- twitter_elections/labeliser.py
+++ twitter_elections/labeliser.py
@@ -5,17 +5,39 @@ import pymongo as pym
 afficher les tweets un par un,  
 prendre en input le sentiment
 insérer le tweet labelisé dans une autre collection'''
+
+def removeDuplicates():
+	client = pym.MongoClient()
+        c = client.tweet.tweet
+	duplicates = []
+	removepipe = [{"$group":{"_id":"$t_id", "dups":{"$push":"$_id"},"count":{"$sum":1}}},{"$match":{"count":{"$gt":1}}}]
+	try :
+		for doc in c.aggregate(removepipe) :
+			it = iter(doc['dups'])
+			next(it)
+			for id in it :
+				duplicates.append(pym.DeleteOne({'_id':id}))
+		c.bulk_write(duplicates)	
+	except:
+		pass
+        client.close()
+
+print("Removing duplicates....\n")
+removeDuplicates()
+print("Done\n")
+
 f = open('indexlabelised','r')
-session = int(input("Combien de tweets à labeliser pour cette session ?"))
+session = int(input("Combien de tweets à labeliser pour cette session ?\n"))
 if session == 0 : exit()
-index = int(f.read()[0])
+index = int(f.read())
 f.close()
+print("Index : " + str(index) +'\n')
 client = pym.MongoClient('localhost',27017)
 collection = client.tweet.tweet
-corpus = collection.find({'t_text':{'$regex':'^(?!.*rt).*$'}},{'t_text':1})[index:index+session]
+corpus = collection.find({'t_text':{'$regex':'^(?!.*rt).*$'}},{'t_text':1}).sort('_id',1)[index+1:index+session]
 client.close()
 sentimentmap = {'a':1,'z':0,'e':-1}
-for i, tweet in enumerate(corpus[:10]):
+for i, tweet in enumerate(corpus):
     currtweet = {}
     print(tweet['t_text'])
     sentiment = raw_input('Sentiment ? Positif : a , Négatif : e, Neutre : z ')
