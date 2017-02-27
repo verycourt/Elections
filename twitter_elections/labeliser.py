bbcfd2d0582e57088d5c3ffbca08b18b46753b67
rvi008 <root@ip-172-31-25-255.ec2.internal>
2017-02-27 13:51:34 +0000

    changed letters for sentiment and port for insertion

--- twitter_elections/labeliser.py
+++ twitter_elections/labeliser.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import pymongo as pym
 
 ''' Récupérer X tweets à labelliser
@@ -8,21 +9,22 @@ f = open('indexlabelised','r')
 session = int(input("Combien de tweets à labeliser pour cette session ?"))
 index = int(f.read()[0])
 f.close()
-client = pym.MongoClient('localhost',27018)
+client = pym.MongoClient('localhost',27017)
 collection = client.tweet.tweet
 corpus = collection.find({'t_text':{'$regex':'^(?!.*rt).*$'}},{'t_text':1})[index:index+session]
 client.close()
+sentimentmap = {'a':1,'z':0,'e':-1}
 for i, tweet in enumerate(corpus[:10]):
     print(tweet['t_text'])
-    tweet['sentiment'] = input('Sentiment ? Positif : a , Négatif : z, Neutre : e ')
+    tweet['sentiment'] = input('Sentiment ? Positif : a , Négatif : e, Neutre : z ')
     while(tweet['sentiment'] not in ['a','z','e']) :
         print("erreur\n")
-        tweet['sentiment'] = input('Sentiment ? Positif : a , Négatif : z, Neutre : e ')
-    labeled = {'text':tweet['t_text'],'sentiment':tweet['sentiment']}
+        tweet['sentiment'] = input('Sentiment ? Positif : a , Négatif : e, Neutre : z ')
+    labeled = {'text':tweet['t_text'],'sentiment':sentimentmap[tweet['sentiment']]}
     f = open('indexlabelised','w')
     f.write(str(int(index)+i))
     f.close()
-    client = pym.MongoClient('localhost',27018)
+    client = pym.MongoClient('localhost',27017)
     collection = client.tweet.train
     collection.insert_one(labeled)
     client.close()
