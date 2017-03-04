bcf14fe3cd9e8a595010716c127e2269d1a059f1
rvi008 <root@ip-172-31-25-255.ec2.internal>
2017-03-04 08:18:58 +0000

    sponsoring

---
+++ parrainages/parrainages.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+import pandas as pd
+url = "https://presidentielle2017.conseil-constitutionnel.fr/les-parrainages/parrainages-par-candidat/"
+
+
+def loadHTML(url):
+    resultats = requests.get(url)
+    return BeautifulSoup(resultats.text, 'html.parser')
+
+def extractSponsoring(url):
+    table = loadHTML(url).find("table")
+    rows = table.findAll('tr')
+    candOfInterest = ['ARTHAUD','MACRON','MELENCHON','LE PEN','HAMON','FILLON','DUPONT-AIGNAN','POUTOU','JUPPE']
+    candidates = []
+    for row in rows[1:] :
+        currcand = {}
+        name = row .findAll('td')[0].text.split(' ')[0]
+        if name not in candOfInterest : continue
+        currcand['Nom'] = name
+        currcand['01/03/2017'] =  row.findAll('td')[2].text
+        currcand['03/03/2017'] =  row.findAll('td')[3].text
+        currcand['07/03/2017'] =  row.findAll('td')[4].text
+        currcand['10/03/2017'] =  row.findAll('td')[5].text
+        currcand['14/03/2017'] =  row.findAll('td')[6].text
+        currcand['18/03/2017'] =  row.findAll('td')[7].text
+        candidates.append(currcand)
+    return candidates
+
+dump = pd.DataFrame(extractSponsoring(url))
+dump.set_index('Nom', inplace=True)
+dump.to_json('/var/www/html/parrainages/parrainages.json', orient='split')
+
