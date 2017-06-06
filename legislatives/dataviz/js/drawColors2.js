0df35c48ccebaedb799f44ede4b074e23b2262f7
raphael <raphael.vignes@telecom-paristech.fr>
2017-06-06 23:12:09 +0200

    les hachures ne marchent pas

--- legislatives/dataviz/js/drawColors2.js
+++ legislatives/dataviz/js/drawColors2.js
@@ -1,7 +1,7 @@
 var svg2 = d3.select("svg#second")
 var dataset2 = []
-var dicoNuances = {"EXG":"#d30202", "COM":"#ff1616", "FI":"#ff1616","SOC":"#f76060","RDG":"#edafaf",
-"ECO":"#41992f","DIV":"#d3913b","REG":"#54422b","REM":"#ffbf00","MDM":" #cca300","UDI":"#536cad","LR":"#3c589e",
+var dicoNuances2 = {"EXG":"#d30202", "COM":"#ff1616", "FI":"#ff1616","SOC":"#f76060","RDG":"#edafaf",
+"ECO":"#41992f","DIV":"#d3913b","REG":"#54422b","REM":"#ffbf00","MDM":"#ed9302","UDI":"#536cad","LR":"#3c589e",
 "DVD":"#1a3372","DLF":"#7928b7","FN":"#03194f","EXD":"#000a23","DVG":"#c66b9a"}
 
 d3.csv('data/resultats2.csv')
@@ -28,7 +28,7 @@ d3.csv('data/resultats2.csv')
 function colorMap(){
 	for(i=0;i < dataset2.length; i++){
 		svg2.select('[id='+'"'+dataset2[i].circo+'"'+']')
-		.style("fill", dicoNuances[dataset2[i].color1])
+		.style("fill", dicoNuances2[dataset2[i].color1])
 		.select("title").text(function(){
 			return dataset2[i].nom +'\n' + dataset2[i].candidat1 + " : " + dataset2[i].score1 * 100 +"%" + "(Vainqueur)" + '\n' 
 		+ dataset2[i].candidat2 + " : " + dataset2[i].score2 * 100 +"%" +'\n' + dataset2[i].candidat3 + " : " + dataset2[i].score3 * 100 +"%"+ '\n';})
