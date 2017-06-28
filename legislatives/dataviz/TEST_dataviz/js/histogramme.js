1981826b68f979b5059d3e33092bf25b2037df61
verycourt <bertrand2vericourt@gmail.com>
2017-06-28 22:08:15 +0200

    dataviz > with b2v's head

--- legislatives/dataviz/TEST_dataviz/js/histogramme.js
+++ legislatives/dataviz/TEST_dataviz/js/histogramme.js
@@ -1,6 +1,6 @@
 var formatCount = d3.format(",.0f");
 
-var svg = d3.select("svg"),
+var svg = d3.select(".histo"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
